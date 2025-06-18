1f8b55b90efd8292b46de7ffc0ebaca89750bf7c
Carlosvillamizar09 <carlosvilla0903@gmail.com>
2025-06-17 22:39:01 -0500

    refactor: :fire: Se cambia lo que se tenia al principio del video al darle siguiente
    Ya que era obsoleto y poco elegante

--- script.js
+++ script.js
@@ -1,19 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
     const siguiente = document.querySelector(".contenido");
-    const videoTransicion = document.getElementById("videoTransicion");
-    const introVideo = document.getElementById("introVideo");
 
     siguiente.addEventListener("click", () => {
-        videoTransicion.style.display = "flex";
-        introVideo.currentTime = 0;
-        introVideo.muted = false; // Cambia a true si no quieres sonido
-        introVideo.play();
-
-        introVideo.addEventListener("ended", () => {
-            videoTransicion.style.animation = "fadeOut 1s ease-in-out forwards";
-            setTimeout(() => {
-                window.location.href = "inicio.html";
-            }, 1000); // Espera a que termine el fade antes de redirigir
-        });
+        window.location.href = "inicio.html";
     });
 });
+
